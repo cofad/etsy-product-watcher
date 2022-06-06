182e0febc8ffaa53c90b051faf990df6b8e50bc0
Will Warner <47200097+cofad@users.noreply.github.com>
2022-06-06 11:11:58 +0000

    move files to src folder

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-deno run --allow-env --allow-net=:8080,smtp.gmail.com:465,openapi.etsy.com --watch ./main.ts
+deno run --allow-env --allow-net=:8080,smtp.gmail.com:465,openapi.etsy.com --watch ./src/main.ts
