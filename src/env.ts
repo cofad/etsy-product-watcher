92216741d203427ee03518f12a4bef199062f849
Will Warner <47200097+cofad@users.noreply.github.com>
2022-07-01 13:38:21 +0000

    fix hardcoded erro to be dynamic

--- src/env.ts
+++ src/env.ts
@@ -2,7 +2,7 @@ export function getEnvOrThrow(envName: string): string {
   const env = Deno.env.get(envName);
 
   if (!env) {
-    throw new Error("Unable to read GMAIL_PASSWORD from env!");
+    throw new Error("Unable to read ${envName} from env!");
   }
 
   return env;
